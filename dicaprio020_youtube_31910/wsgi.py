9acef2aa79a8fd294b26118a5654d62a57e5561d
Crowdbotics <team@crowdbotics.com>
2021-11-10 15:22:03 +0000

    Update models

--- dicaprio020_youtube_31910/wsgi.py
+++ dicaprio020_youtube_31910/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dicaprio020_youtube_31910.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dicaprio020_youtube_31910.settings")
 
 application = get_wsgi_application()
